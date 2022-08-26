0930bac1358c38e90b32393bf9bd7a988c172408
ptqpro <111232795+ptqpro@users.noreply.github.com>
2022-08-26 21:39:47 +0700

    Add files via upload

---
+++ Set-uptermux.py
@@ -0,0 +1,4 @@
+import os,sys
+
+os.system("termux-setup-storage && apt update && apt upgrade && pkg install php && pkg install python && pkg install git && pkg install wget && pip install --upgrade pip && pip install requests && pip install pycurl")
+os.system('clear')
